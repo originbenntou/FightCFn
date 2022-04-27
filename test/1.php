406c957e5a6d8e6e5f4009594d2d58e64330c442
originbenntou <originbenntou8973@gmail.com>
2022-04-27 23:48:55 +0900

    save

--- test/1.php
+++ test/1.php
@@ -1,9 +1,7 @@
 <?php
 
-$survives = explode(" ", trim(fgets(STDIN)));
-$capacity = intval(trim(fgets(STDIN)));
-
-rsort($survives);
+$survives = explode(" ", trim(fgets(STDIN))); // 生き残り
+$capacity = intval(trim(fgets(STDIN))); // ボート空き容量
 
 $resqueState = [
     "survives" => $survives,
@@ -12,60 +10,103 @@ $resqueState = [
     "remains" => []
 ];
 
+// 体重降順
+// 一番重い人がボートに乗れなければ確実に「ピンチ！」
+// そうじゃなければ全員脱出できる
+rsort($survives);
+
 if (max($survives) > $capacity) {
     echo "ピンチ！" . "\n";
 } else {
     echo resqueAttempt($resqueState, 0) . "\n";
 }
 
+/*
+ * 救助施行
+ * 出航回数＝ボートの数を返却
+ */
 function resqueAttempt($resqueState, $sailCount = 0) {
+    // 再帰関数ストッパー
+    // 生き残りがいなくなるまで再帰呼び出しが続く
     if (count($resqueState["survives"]) === 0) {
 
+        // ボードに誰か乗っていたら出航
         if (count($resqueState["riders"]) > 0) {
             $resqueState = sail($resqueState);
             $sailCount++;
         }
 
+        // 居残りがまだ入れば救助は完了していない
+        // 再実行
         if (count($resqueState["remains"]) > 0) {
             $resqueState = reRescue($resqueState);
             return resqueAttempt($resqueState, $sailCount);
         }
 
+        // 生き残りがいない、居残りもいないなら救助完了
         return $sailCount;
     }
 
+    // ボートの空き容量が最も少なくなるように計算
+    // まず生き残りの中でいちばん重い人からボートに乗る
     $survive = array_shift($resqueState["survives"]);
 
+    // 体重を測ってボートに乗る
+    // 乗れなかったら居残り
     if ($survive <= $resqueState["boatCapacity"]) {
         $resqueState = ride($resqueState, $survive);
     } else {
         $resqueState = remain($resqueState, $survive);
     }
 
+    // 再帰呼び出し
     return resqueAttempt($resqueState, $sailCount);
 }
 
-function ride($resqueState, $survive) {
+// 救助状態を操作する関数群
+
+/*
+ * ライド
+ * 生き残りをボートに移動して、ボートの空き容量を減らす
+ */
+function ride($resqueState, $survive): array
+{
     $resqueState["boatCapacity"] = $resqueState["boatCapacity"] - $survive;
     $resqueState["riders"][] = $survive;
 
     return $resqueState;
 }
 
-function remain($resqueState, $survive) {
+
+/*
+ * 居残り
+ * 生き残りを居残り組に移動する
+ */
+function remain($resqueState, $survive): array
+{
     $resqueState["remains"][] = $survive;
 
     return $resqueState;
 }
 
-function sail($resqueState) {
+/*
+ * 出航
+ * ボートの空き容量と乗っている人をもとに戻して、新しいボートの状態にする
+ */
+function sail($resqueState): array
+{
     $resqueState["boatCapacity"] = array_sum($resqueState["riders"]) + $resqueState["boatCapacity"];
     $resqueState["riders"] = [];
 
     return $resqueState;
 }
 
-function reRescue($resqueState) {
+/*
+ * 再救助
+ * 居残り組を再度生き残りにする
+ */
+function reRescue($resqueState): array
+{
     $resqueState["survives"] = $resqueState["remains"];
     $resqueState["remains"] = [];
 
--- test/2.php
+++ test/2.php
@@ -1,9 +1,12 @@
 <?php
 
+// 茶色と緑の格子数
 [$brown, $green] = explode(" ", trim(fgets(STDIN)));
 
+// 緑格子を四角形に並べたとき縦横それぞれの枚数をペアにしてリスト
+// 緑格子の縦が1枚,2枚...と増やしていき、横の枚数を算出
+// 縦横の組み合わせはどこかで繰り返しになるため、縦の枚数を増やすのは合計枚数の平方根まで
 $greenRowColumn = [];
-
 for ($i = 1; $i <= sqrt($green); $i++) {
     if (($green % $i) === 0) {
         $greenRowColumn[] = [$i, $green / $i];
@@ -12,11 +15,17 @@ for ($i = 1; $i <= sqrt($green); $i++) {
 
 $answer = [];
 foreach ($greenRowColumn as $queue) {
-    [$row, $column] = $queue;
+    [$greenRow, $greenColumn] = $queue;
+    $sum = $green + $brown;
 
-    for ($i = $row + 2; $i <= floor($brown + $green / ($column + 2)); $i = $i + 2) {
-        for ($j = $column + 2; $j <= floor($brown + $green / $i); $j = $j + 2) {
-            if ((($i * $j) - $green) == $brown) {
+    // 茶色格子は緑格子を囲むように並べる
+    // 四角形になるように並べるので縦列×横列が格子の合計となる、2重ループでそれを見つける
+    // 縦格子を増やせる最大は、格子の合計を横格子の枚数（最小）で割った商
+    // 横格子を増やせる最大は、格子の合計を縦格子の枚数で割った商
+    // 商のあまりが出る場合、縦×横=合計枚数にはならないので無視できる
+    for ($i = $greenRow + 2; $i <= floor($sum / ($greenColumn + 2)); $i = $i + 2) {
+        for ($j = $greenColumn + 2; $j <= floor($sum / $i); $j = $j + 2) {
+            if (($i * $j) == $sum) {
                 $answer = [$i, $j];
                 break;
             }
